c3a20823607d713515228d684bbba15128124704
Andong LIAO <liaoad_space@sina.com>
2018-12-24 14:21:38 +0800

    support more theme

--- src/components/inlineNotification/__tests__/inlineNotification.test.tsx
+++ src/components/inlineNotification/__tests__/inlineNotification.test.tsx
@@ -1,3 +1,4 @@
+import { Icon, NotificationTheme } from "@src/components";
 import { shallow } from "enzyme";
 import * as React from "react";
 import { InlineNotification } from "../InlineNotification";
@@ -5,19 +6,27 @@ import { InlineNotification } from "../InlineNotification";
 describe("Inline notification", () => {
   it("should render warning notification success", () => {
     const wrapper = shallow(
-      <InlineNotification text="this is warning text" type="warning" />
+      <InlineNotification
+        text="this is warning text"
+        theme={NotificationTheme.Warning}
+      />
     );
 
     expect(wrapper.find("p").text()).toEqual("this is warning text");
-    expect(wrapper.find(".warning")).toHaveLength(2);
+    expect(wrapper.find(".warning")).toHaveLength(1);
+    expect(wrapper.find(Icon).prop("type")).toEqual("warning");
   });
 
   it("should render error notification success", () => {
     const wrapper = shallow(
-      <InlineNotification text="this is error text" type="error" />
+      <InlineNotification
+        text="this is error text"
+        theme={NotificationTheme.Error}
+      />
     );
 
     expect(wrapper.find("p").text()).toEqual("this is error text");
-    expect(wrapper.find(".error")).toHaveLength(2);
+    expect(wrapper.find(".error")).toHaveLength(1);
+    expect(wrapper.find(Icon).prop("type")).toEqual("notification-error");
   });
 });
