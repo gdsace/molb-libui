e22ca3d074c547f54ac76b4971414ecab76c9b1f
fanyangxi <fanyangxi@live.cn>
2018-12-27 11:50:38 +0800

    [fix] update checkbox stories

--- src/stories/checkbox/checkbox.stories.tsx
+++ src/stories/checkbox/checkbox.stories.tsx
@@ -5,6 +5,8 @@ import { Checkbox } from "@src/components";
 import { storiesOf } from "@storybook/react";
 import { wInfo } from "../utils";
 
+const styles = require("./checkbox.stories.scss");
+
 const store = new Store({
   checked: false
 });
@@ -17,24 +19,36 @@ const onCheckboxClick = (value: string) => {
   "Checkbox",
   wInfo(``)(() => {
     return (
-      <div>
-        <State store={store}>
-          <Checkbox
-            checked={store.checked}
-            disabled={false}
-            onCheckboxClick={onCheckboxClick}
-          />
-        </State>
-        <Checkbox
-          checked={false}
-          disabled={true}
-          onCheckboxClick={onCheckboxClick}
-        />
-        <Checkbox
-          checked={true}
-          disabled={true}
-          onCheckboxClick={onCheckboxClick}
-        />
+      <div className={styles.rootContainer}>
+        <h6 className={styles.groupHeader}>Checkbox: ...</h6>
+        <div className={styles.itemsContainer}>
+          <div className={styles.box}>
+            <p className={styles.notes}>Normal:</p>
+            <State store={store}>
+              <Checkbox
+                checked={store.checked}
+                disabled={false}
+                onCheckboxClick={onCheckboxClick}
+              />
+            </State>
+          </div>
+          <div className={styles.box}>
+            <p className={styles.notes}>Disabled:</p>
+            <Checkbox
+              checked={false}
+              disabled={true}
+              onCheckboxClick={onCheckboxClick}
+            />
+          </div>
+          <div className={styles.box}>
+            <p className={styles.notes}>Disabled: and Checked</p>
+            <Checkbox
+              checked={true}
+              disabled={true}
+              onCheckboxClick={onCheckboxClick}
+            />
+          </div>
+        </div>
       </div>
     );
   })
