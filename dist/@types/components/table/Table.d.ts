07034116440975bc77cc80c249b57c839fad057b
Lingwei Wang <234443837@qq.com>
2019-01-03 14:54:58 +0800

    update dist

--- dist/@types/components/table/Table.d.ts
+++ dist/@types/components/table/Table.d.ts
@@ -9,9 +9,21 @@ export interface IDateSource {
 export interface ITableProps {
     dataSource: IDateSource[];
     columns: IColumn[];
-    style?: object;
+    tableCls?: string;
+    bordered?: boolean;
+    size?: TableSize;
+    theme?: TableTheme;
+}
+export declare enum TableSize {
+    Small = "small",
+    Large = "large"
+}
+export declare enum TableTheme {
+    Striped = "striped",
+    Basic = "basic"
 }
 export declare class Table extends React.Component<ITableProps, {}> {
+    static defaultProps: Partial<ITableProps>;
     render(): JSX.Element;
     getBodyComponent(columns: IColumn[], dataSource: IDateSource[]): React.ReactNode;
     getHeadComponent(columns: IColumn[]): React.ReactNode;
