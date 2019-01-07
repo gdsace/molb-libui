bf46eb111d9184ed2725e2271358c21853ea4602
WuYifanX <510182912@qq.com>
2019-01-07 13:52:38 +0800

    [fix] * rename the buttonIcon to iconButton

--- dist/@types/components/iconButton/IconButton.d.ts
+++ dist/@types/components/iconButton/IconButton.d.ts
@@ -1,12 +1,12 @@
 import * as React from "react";
 import { IIconProps } from "../icons";
-export interface IButtonIconProps extends IIconProps {
+export interface IIconButtonProps extends IIconProps {
     onClick: () => any;
     disabled?: boolean;
     iconClassName?: string;
 }
-export declare class ButtonIcon extends React.Component<IButtonIconProps, {}> {
-    static defaultProps: Partial<IButtonIconProps>;
+export declare class IconButton extends React.Component<IIconButtonProps, {}> {
+    static defaultProps: Partial<IIconButtonProps>;
     render(): JSX.Element;
     private handleOnClick;
 }
