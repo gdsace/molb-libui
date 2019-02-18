e1a90f62e1e4108ced1023e1bf3c8bb0aea09975
Lingwei Wang <234443837@qq.com>
2019-02-18 16:11:49 +0800

    [feature] display tooltip in checkboxTypeQuestion

--- dist/@types/components/checkboxTypeQuestion/CheckboxTypeQuestion.d.ts
+++ dist/@types/components/checkboxTypeQuestion/CheckboxTypeQuestion.d.ts
@@ -5,6 +5,7 @@ export interface ICheckboxTypeQuestion {
     onCheckboxClick: any;
     questionLabel: string;
     questionDescription?: string;
+    tooltip?: string;
 }
 export declare class CheckboxTypeQuestion extends React.Component<ICheckboxTypeQuestion, {}> {
     render(): JSX.Element;
