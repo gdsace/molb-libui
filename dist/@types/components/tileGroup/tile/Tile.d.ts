c4fde74a28dc62fe5f262f30f1099dec2518a3ee
dongjx <annie.djx@gmail.com>
2019-02-22 11:44:01 +0800

    [chore] build production dist

---
+++ dist/@types/components/tileGroup/tile/Tile.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="react" />
+import { TileTheme } from "../../EnumValues";
+export interface ITileProps {
+    icon?: string;
+    content: string;
+    subContent?: string;
+    description?: string;
+    checked?: boolean;
+    onChange?: (e: any) => void;
+    disabled?: boolean;
+    value?: string;
+    theme?: TileTheme;
+    containerStyle?: string;
+    error?: string;
+    deselectable?: boolean;
+    imgSrc?: string;
+    imgAlt?: string;
+}
+export declare const Tile: (props: ITileProps) => JSX.Element;
