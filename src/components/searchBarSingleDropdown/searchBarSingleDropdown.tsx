082a26262d32625a52349cbdc6108ab4dd3b6c13
Rui <rchai@palo-it.com>
2019-05-16 15:58:28 +0800

    created a new component for libui

---
+++ src/components/searchBarSingleDropdown/searchBarSingleDropdown.tsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { Dropdown, Button, Input, InputType, Size, Theme } from "../../components";
+
+const styles = require("./searchBarSingleDropdown.scss");
+
+export interface IDropdownOptionType {
+  value: string;
+  label: string;
+}
+
+export interface ISearchSingleDropdownProps {
+  //dropdown props
+  dropdownOptions?: IDropdownOptionType[];
+  selectedDropdown: IDropdownOptionType;
+  handleDropdownChange: (optionValue: IDropdownOptionType) => any;
+
+  //input props
+  inputText: string;
+  handleInputChange: (inputText: string) => any;
+  inputPlaceholder?: string;
+  inputMaxlength?: number;
+
+  //button props
+  buttonLabel: string;
+  handleButtonClick: (inputText: string, selectedDropdown: IDropdownOptionType) => any;
+}
+
+export class SearchSingleDropdown extends React.Component<
+  ISearchSingleDropdownProps,
+  {}
+> {
+  public render() {
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.searchSection}>
+          <Dropdown
+            options={this.props.dropdownOptions}
+            className={styles.searchTypeDropdown}
+            value={this.props.selectedDropdown}
+            onChange={(optionValue) => {
+              const option = optionValue as IDropdownOptionType;
+              this.props.handleDropdownChange(option);
+            }}
+            textInputValue={this.props.selectedDropdown.label}
+          />
+          <div className={styles.searchTextInputWrapper}>
+            <Input
+              size={Size.Large}
+              type={InputType.Text}
+              value={this.props.inputText}
+              placeholder={this.props.inputPlaceholder}
+              maxLength={this.props.inputMaxlength || 100} 
+              onChange={(e) => {
+                this.props.handleInputChange(e.target.value);
+              }}
+              // TODO pageConst
+            />
+          </div>
+          <Button 
+            className={styles.searchButton}
+            label={this.props.buttonLabel}
+            size={Size.Medium}
+            theme={Theme.Primary}
+            type="button"
+            onClick={() => {
+              this.props.handleButtonClick(this.props.inputText, this.props.selectedDropdown);
+            }} 
+            // TODO pageConst.SEARCH_APPLICATIONS
+          />
+        
+        </div>
+      </div>
+    );
+  }
+}
