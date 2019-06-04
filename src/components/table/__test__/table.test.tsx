69f9bd27a70cfc3c14ed1521055e33f531ac9a57
Jonathan Tan <jontan@palo-it.com>
2019-06-04 11:07:25 +0800

    Added test to check that table row is clickable

--- src/components/table/__test__/table.test.tsx
+++ src/components/table/__test__/table.test.tsx
@@ -35,4 +35,57 @@ describe("Table", () => {
     const tBodyText = wrapper.find("tbody").text();
     expect(tBodyText).toEqual("Joe Black32Joe Black33");
   });
+
+  describe("when configured to be row clickable", () => {
+    let rfaDataSource: {};
+
+    beforeEach(() => {
+      rfaDataSource = [
+        {
+          key: "1",
+          name: "Joe Black",
+          age: 32,
+          address: "Sidney No. 1 Lake Park",
+          onRowclickHandler: jest.fn()
+        }
+      ];
+    });
+
+    it("should allow its row to be clickable if [clickableRow] is set to true", () => {
+      const wrapper = Enzyme.shallow(
+        <Table
+          columns={tableColumns}
+          dataSource={rfaDataSource}
+          clickableRow={true}
+        />
+      );
+
+      wrapper
+        .find("tbody")
+        .find("tr")
+        .simulate("click");
+
+      // @ts-ignore
+      expect(rfaDataSource[0].onRowclickHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it("else it should not be clickable", () => {
+      const wrapper = Enzyme.shallow(
+        <Table
+          columns={tableColumns}
+          // @ts-ignore
+          dataSource={rfaDataSource}
+          clickableRow={false}
+        />
+      );
+
+      wrapper
+        .find("tbody")
+        .find("tr")
+        .simulate("click");
+
+      // @ts-ignore
+      expect(rfaDataSource[0].onRowclickHandler).toHaveBeenCalledTimes(0);
+    });
+  });
 });
