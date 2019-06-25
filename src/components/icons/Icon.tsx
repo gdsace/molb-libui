3f2e33e3ba16fbfe34afc61b2ffe0ec9dad30c69
elkooy <ekooy@palo-it.com>
2019-06-25 17:12:57 +0800

    yarn lint

--- src/components/icons/Icon.tsx
+++ src/components/icons/Icon.tsx
@@ -22,7 +22,8 @@ export const Icon = (props: IIconProps) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
-      className={`dib v-mid ${props.type != "delete" && styles.pointerEvent} ${props.className || ""}`}
+      className={`dib v-mid ${props.type !== "delete" &&
+        styles.pointerEvent} ${props.className || ""}`}
       width={props.size || "24"}
       height={props.size || "24"}
       viewBox={props.viewBox || `0 0 24 24`}
