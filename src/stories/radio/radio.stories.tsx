840204ef829bb78aa48cea77924b8778e2ddb7ec
JellyLu <yjlu@thoughtworks.com>
2019-10-03 14:37:26 +0800

    [#168422236] Refactor radio stories with Knobs, so that user can customize props.
    Co-Authored-By: Muthu<muthu_karuppi_palaniappan@tech.gov.sg>

--- src/stories/radio/radio.stories.tsx
+++ src/stories/radio/radio.stories.tsx
@@ -1,14 +1,13 @@
-import React from "react";
-
-import { State, Store } from "@sambego/storybook-state";
+import { boolean, object, text } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
-import { IOptionValue, IRadioProps, Radio } from "../../components/radio";
-import { CategoryName, wInfo } from "../utils";
+import { noop } from "lodash";
+import React from "react";
+import { IOptionValue, Radio } from "../../components/radio";
+import { CategoryName } from "../utils";
 const styles = require("./radio.scss");
 
-const valueChangeHandler = (value: string) => {
-  store.set({ value });
-};
+const valueChangeHandler = noop;
+
 const optionValueArray: IOptionValue[] = [
   {
     value: "value1",
@@ -19,42 +18,6 @@ const optionValueArray: IOptionValue[] = [
     label: "no"
   }
 ];
-
-const radioProps: IRadioProps = {
-  text: "1. all enable radio buttons",
-  optionList: optionValueArray,
-  onChange: valueChangeHandler,
-  disabled: false,
-  showError: true,
-  errorMsg: "This field is required.",
-  radioLabelLineBreak: false,
-  showTooltip: true
-};
-
-const valueChangeHandler2 = (value: string) => {
-  store2.set({ value });
-};
-const optionValueArray2: IOptionValue[] = [
-  {
-    value: "value1",
-    label: "yes"
-  },
-  {
-    value: "value2",
-    label: "no"
-  }
-];
-
-const radioProps2: IRadioProps = {
-  text: "2. all disabled radio buttons",
-  optionList: optionValueArray2,
-  onChange: valueChangeHandler2,
-  disabled: true,
-  value: "value1",
-  radioTextStyleOverride: styles.overrideRadioText,
-  labelStyleOverride: styles.overrideRadioLabel,
-  radioLabelLineBreak: false
-};
 
 const optionValueArray3: IOptionValue[] = [
   {
@@ -72,21 +35,6 @@ const optionValueArray3: IOptionValue[] = [
   }
 ];
 
-const valueChangeHandler3 = (value: string) => {
-  store3.set({ value });
-};
-const radioProps3: IRadioProps = {
-  text: "3. mixed disabled and enabled radio buttons",
-  optionList: optionValueArray3,
-  onChange: valueChangeHandler3,
-  disabled: false,
-  value: "value1",
-  radioLabelLineBreak: false
-};
-
-const valueChangeHandler4 = (value: string) => {
-  store4.set({ value });
-};
 const optionValueArray4: IOptionValue[] = [
   {
     value: "value1",
@@ -99,126 +47,108 @@ const optionValueArray4: IOptionValue[] = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum, tellus ut dapibus scelerisque, leo purus consectetur eros, eget tincidunt ipsum turpis et quam. Cras cursus sit amet ante ut pellentesque. Nam lobortis porttitor nisi et vestibulum. Curabitur ac ligula sit amet lorem iaculis porttitor eu sed nunc. Etiam id eleifend ligula, a vehicula mauris. Suspendisse at eros gravida metus sagittis luctus. Maecenas pellentesque, magna sit amet rhoncus rhoncus, est lacus interdum risus, sed auctor lacus felis vel libero."
   }
 ];
-
-const radioProps4: IRadioProps = {
-  text: "4. ensure line break for each radio button",
-  optionList: optionValueArray4,
-  onChange: valueChangeHandler4,
-  disabled: false,
-  showError: true,
-  radioLabelLineBreak: true
-};
-
-const valueChangeHandler5 = (value: string) => {
-  store5.set({ value });
-};
-
-const optionValueArray5: IOptionValue[] = [
-  {
-    value: "value1",
-    label: "first"
-  },
-  {
-    value: "value2",
-    label: "second"
-  }
-];
-
-const radioProps5: IRadioProps = {
-  text: "5. radio button with tooltip",
-  optionList: optionValueArray5,
-  onChange: valueChangeHandler5,
-  disabled: false,
-  showError: false,
-  errorMsg: "This field is required.",
-  radioLabelLineBreak: false,
-  showTooltip: true,
-  toolTipsContent: "This is the content you want to show on tooltip"
-};
-
-const radioProps6: IRadioProps = {
-  text: "6. radio button with label and prompt message",
-  optionList: optionValueArray5,
-  onChange: valueChangeHandler5,
-  disabled: false,
-  radioLabelLineBreak: false,
-  label: "LIQUOR",
-  promptMessage: {
-    display: true,
-    message: "this is the prompt message"
-  }
-};
-
-const radioProps7: IRadioProps = {
-  text: "7. radio button with label and addon below label",
-  optionList: optionValueArray5,
-  onChange: valueChangeHandler5,
-  disabled: false,
-  radioLabelLineBreak: false,
-  label: "LIQUOR",
-  addOnBelowText: <div> ------------ </div>
-};
-
-/**
- * Here define store, we can define our state here.
- * If we define store here, the same property in props won't work.
- */
-const store = new Store({
-  value: ""
-});
-const store2 = new Store({
-  // if we have the value in the state, the value in props won't work.
-  value: "value1"
-});
-const store3 = new Store({
-  value: ""
-});
-const store4 = new Store({
-  value: ""
-});
-
-const store5 = new Store({
-  value: ""
-});
-
-const store6 = new Store({
-  value: ""
-});
-
-const store7 = new Store({
-  value: ""
-});
-
 /**
  * State component can use store as a component.
  * Then the stateless component Dropdown get update
  * from store.get("value")
  */
-(storiesOf(CategoryName.SelectionControls, module) as any).addWithJSX(
-  "Radio",
-  wInfo(``)(() => (
-    <div>
-      <State store={store}>
-        <Radio {...radioProps} />
-      </State>
-      <State store={store2}>
-        <Radio {...radioProps2} />
-      </State>
-      <State store={store3}>
-        <Radio {...radioProps3} />
-      </State>
-      <State store={store4}>
-        <Radio {...radioProps4} />
-      </State>
-      <State store={store5}>
-        <Radio {...radioProps5} />
-      </State>
-      <State store={store6}>
-        <Radio {...radioProps6} />
-      </State>
-      <State store={store7}>
-        <Radio {...radioProps7} />
-      </State>
-    </div>
+(storiesOf(CategoryName.SelectionControls, module) as any)
+  .addWithJSX("Radio showing errors", () => (
+    <Radio
+      text={text("text", "1. all enable radio buttons")}
+      optionList={optionValueArray.map((opt, index) =>
+        object(`optionItem[${index}]`, opt)
+      )}
+      onChange={valueChangeHandler}
+      disabled={boolean("disabled", false)}
+      showError={boolean("showError", true)}
+      errorMsg={text("errorMsg", "This field is required.")}
+      radioLabelLineBreak={boolean("radioLabelLineBreak", false)}
+      showTooltip={boolean("showTooltip", false)}
+    />
   ))
-);
+  .addWithJSX("Radio Disabled", () => (
+    <Radio
+      text={text("text", "2. all disabled radio buttons")}
+      optionList={optionValueArray.map((opt, index) =>
+        object(`optionItem[${index}]`, opt)
+      )}
+      onChange={valueChangeHandler}
+      disabled={boolean("disabled", true)}
+      value={text("value", "value1")}
+      radioTextStyleOverride={styles.overrideRadioText}
+      labelStyleOverride={styles.overrideRadioLabel}
+      radioLabelLineBreak={boolean("radioLabelLineBreak", false)}
+    />
+  ))
+  .addWithJSX("Radio with enabled and disabled", () => (
+    <Radio
+      text="3. mixed disabled and enabled radio buttons"
+      optionList={optionValueArray3.map((opt, index) =>
+        object(`optionItem[${index}]`, opt)
+      )}
+      onChange={valueChangeHandler}
+      disabled={boolean("disabled", false)}
+      value={text("value", "value1")}
+      radioLabelLineBreak={boolean("radioLabelLineBreak", false)}
+    />
+  ))
+  .addWithJSX("Radio with line break", () => (
+    <Radio
+      text="4. ensure line break for each radio button"
+      optionList={optionValueArray4.map((opt, index) =>
+        object(`optionItem[${index}]`, opt)
+      )}
+      onChange={valueChangeHandler}
+      disabled={boolean("disabled", false)}
+      showError={boolean("showError", true)}
+      radioLabelLineBreak={boolean("radioLabelLineBreak", true)}
+    />
+  ))
+  .addWithJSX("Radio with tooltip", () => (
+    <Radio
+      text={text("text", "5. radio button with tooltip")}
+      optionList={optionValueArray.map((opt, index) =>
+        object(`optionItem[${index}]`, opt)
+      )}
+      onChange={valueChangeHandler}
+      disabled={boolean("disabled", false)}
+      showError={boolean("showError", false)}
+      errorMsg="This field is required."
+      radioLabelLineBreak={boolean("radioLabelLineBreak", false)}
+      showTooltip={boolean("showTooltip", true)}
+      toolTipsContent={text(
+        "toolTipsContent",
+        "This is the content you want to show on tooltip"
+      )}
+    />
+  ))
+  .addWithJSX("Radio with prompt message", () => (
+    <Radio
+      text={text("text", "6. radio button with label and prompt message")}
+      optionList={optionValueArray.map((opt, index) =>
+        object(`optionItem[${index}]`, opt)
+      )}
+      onChange={valueChangeHandler}
+      disabled={boolean("disabled", false)}
+      radioLabelLineBreak={boolean("radioLabelLineBreak", false)}
+      label={text("label", "LIQUOR")}
+      promptMessage={{
+        display: true,
+        message: "this is the prompt message"
+      }}
+    />
+  ))
+  .addWithJSX("Radio with add below", () => (
+    <Radio
+      text={text("text", "7. radio button with label and addon below label")}
+      optionList={optionValueArray.map((opt, index) =>
+        object(`optionItem[${index}]`, opt)
+      )}
+      onChange={valueChangeHandler}
+      disabled={boolean("disabled", false)}
+      radioLabelLineBreak={boolean("radioLabelLineBreak", false)}
+      label={text("label", "LIQUOR")}
+      addOnBelowText={object("addOnBelowText", <div> ------------ </div>)}
+    />
+  ));
