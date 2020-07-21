429ecf061259485f34c0d890a7c6f35675dd063b
Gabriel Lu <glu@palo-it.com>
2020-07-21 15:31:43 +0800

    Update story text implementation

--- src/stories/notification/InlineNotification.stories.tsx
+++ src/stories/notification/InlineNotification.stories.tsx
@@ -8,11 +8,14 @@ const ThemeList: NotificationTheme[] = Object.keys(NotificationTheme).map(
   k => NotificationTheme[k as keyof typeof NotificationTheme]
 );
 
+const lorem =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit.\nLaboriosam natus doloribus aperiam ipsum consectetur, accusamus laborum nam dolor error consequatur beatae quaerat doloremque placeat impedit illo cumque. Provident, eos aperiam.";
+
 (storiesOf(CategoryName.Notification, module) as any).addWithJSX(
   "InlineNotification",
   () => (
     <InlineNotification
-      text={text("text", "Lorem, ipsum dolor sit amet consectetur adipisicing elit.\nLaboriosam natus doloribus aperiam ipsum consectetur, accusamus laborum nam dolor error consequatur beatae quaerat doloremque placeat impedit illo cumque. Provident, eos aperiam.")}
+      text={text("text", lorem)}
       theme={select("theme", ThemeList, NotificationTheme.Informational)}
     />
   )
