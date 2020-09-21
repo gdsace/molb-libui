721b944f8fa40d8ed7812206571337a99d72cd0e
Muhammad Syafiq Hanafee <sy@fiq.me>
2020-09-21 12:01:40 +0800

    build: again
    it failed on the previous build because my local node_modules were not in sync with the ones in yarn.lock.
    
    In retrospec, there needs to be a tool that checks the versions in yarn.lock with node_modules.

---
+++ dist/@types/components/fileUpload/FileUpload.d.ts
@@ -0,0 +1,36 @@
+import React from "react";
+import { DropzoneProps } from "react-dropzone";
+import { IDocument, IDocumentType } from "../types";
+export declare type IFileUploadProps = Omit<DropzoneProps, "children"> & {
+    baseUrl: string;
+    subjectId: string;
+    token: string;
+    document?: Partial<IDocument>;
+    documentType: IDocumentType;
+    error?: string;
+    linkDescription?: string;
+    className?: string;
+    onSuccess?: (event: any) => any;
+    onError?: (event: any) => any;
+    onCompleteIconClick?: (event: React.MouseEvent, document: Partial<IDocument>) => any;
+    onDefaultIconClick?: (event: React.MouseEvent) => any;
+    onProgressIconClick?: (event: React.MouseEvent) => any;
+    validateFile?: (file: File, documentTypeCode: string) => any;
+};
+export declare type FileUploadState = {
+    fileInfo?: Partial<IDocument>;
+    uploadState: FileUploadStatus;
+};
+export declare enum FileUploadStatus {
+    Complete = "complete",
+    Error = "error",
+    Unstarted = "unstarted",
+    Uploading = "uploading"
+}
+export declare const COMMON_ALLOWED_EXTENSIONS = ".pdf,.png,.jpg,.jpeg";
+export declare class FileUpload extends React.Component<IFileUploadProps, FileUploadState> {
+    state: FileUploadState;
+    UNSAFE_componentWillReceiveProps(nextProps: IFileUploadProps): void;
+    render(): JSX.Element;
+    uploadFile(file: File): void;
+}
