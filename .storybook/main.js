b30cb93367d1dc0bfb3334e6d24520a7960be3fa
Jeremy Yap <jeremy.yapjl@gmail.com>
2020-10-14 12:56:24 +0800

    Migrate to main.js configuration

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ["../**/*.stories.tsx", "welcome.stories.tsx"],
+  addons: [
+    "@storybook/addon-actions",
+    "storybook-addon-jsx",
+    "@storybook/addon-viewport",
+    "@storybook/addon-knobs"
+  ]
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { configure } from "@storybook/react";
 import { addDecorator } from "@storybook/react";
 import { jsxDecorator } from "storybook-addon-jsx";
 import { withKnobs } from "@storybook/addon-knobs/react";
@@ -15,13 +14,3 @@ addDecorator(jsxDecorator);
 // Include global CSS and variables
 import "../src/components/styles/root.scss";
 import "../src/components/styles/fonts.scss";
-
-// automatically import all files ending in *.stories.js
-const req = require.context("../src/stories", true, /.stories.tsx$/);
-
-function loadStories() {
-  require("./welcomeStory.tsx");
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
