c48913c84d7f28a0c67b808f5c4a856625e6784b
Jeremy Yap <jeremy.yapjl@gmail.com>
2020-10-15 18:02:14 +0800

    Update dist

--- dist/@types/stories/g2bDatePicker/G2BDatePicker.stories.d.ts
+++ dist/@types/stories/g2bDatePicker/G2BDatePicker.stories.d.ts
@@ -1,6 +1,11 @@
 /// <reference types="react" />
 import { G2BDatePicker } from "../../components/g2bDatePicker";
-export declare const _G2BDatePicker: () => JSX.Element;
+export declare const _G2BDatePicker: {
+    (): JSX.Element;
+    story: {
+        name: string;
+    };
+};
 declare const _default: {
     title: string;
     component: typeof G2BDatePicker;
